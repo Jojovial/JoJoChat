650007c5f5f97856444357e2d586bd71e4e51278
JoJo <joehearthstone@gmail.com>
2024-06-15 16:04:10 -0400

    refactor client

--- client.py
+++ client.py
@@ -1,17 +1,52 @@
 import socket
+import threading
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def receive_messages(client_socket):
+    while True:
+        try:
+            msg = client_socket.recv(1024).decode('utf-8')
+            if msg.lower() == 'quit':
+                print("Server disconnected")
+                break
+            else:
+                print(f"Server: {msg}")
+        except (ConnectionResetError, ConnectionAbortedError):
+            print("Connection lost")
+            break
+        except Exception as e:
+            print(f"Error: {e}")
+            break
+    client_socket.close()
 
-client.connect(("localhost", 3000))
+def send_messages(client_socket):
+    while True:
+        try:
+            msg = input("Message: ")
+            client_socket.send(msg.encode('utf-8'))
+            if msg.lower() == 'quit':
+                break
+        except (ConnectionResetError, ConnectionAbortedError):
+            print("Connection lost")
+            break
+        except Exception as e:
+            print(f"Error: {e}")
+            break
+    client_socket.close()
 
-done = False
+def start_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(("localhost", 3000))
+    print("Connected to the server")
 
-while not done:
-    client.send(input("Message: ").encode('utf-8'))
-    msg = client.recv(1024).decode('utf-8')
-    if msg == "quit":
-        done = True
-    else:
-        print(msg)
+    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+    send_thread = threading.Thread(target=send_messages, args=(client_socket,))
 
-client.close()
+    receive_thread.start()
+    send_thread.start()
+
+    receive_thread.join()
+    send_thread.join()
+    print("Client disconnected")
+
+if __name__ == "__main__":
+    start_client()
