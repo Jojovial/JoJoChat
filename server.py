a9a0e5ea9b4b2a86bd7bacc4eed5d1455e4d37fb
JoJo <joehearthstone@gmail.com>
2024-06-15 16:03:45 -0400

    refactor server

--- server.py
+++ server.py
@@ -1,21 +1,40 @@
 import socket
+import threading
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("localhost", 3000))
+def handle_client(client_socket):
+    with client_socket:
+        while True:
+            try:
+                msg = client_socket.recv(1024).decode('utf-8')
+                if msg.lower() == 'quit':
+                    print("Client disconnected")
+                    break
+                else:
+                    print(f"Client: {msg}")
+                    client_socket.send(input("Message: ").encode('utf-8'))
+            except (ConnectionResetError, ConnectionAbortedError):
+                print("Connection lost")
+                break
+            except Exception as e:
+                print(f"Error: {e}")
+                break
 
-server.listen()
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("localhost", 3000))
+    server.listen()
+    print("Server listening on port 3000")
 
-client, addr = server.accept()
+    try:
+        while True:
+            client_socket, addr = server.accept()
+            print(f"Connection established with {addr}")
+            client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+            client_handler.start()
+    except KeyboardInterrupt:
+        print("Server shutting down")
+    finally:
+        server.close()
 
-done = False
-
-while not done:
-    msg = client.recv(1024).decode('utf-8')
-    if msg == 'quit':
-        done = True
-    else:
-        print(msg)
-    client.send(input("Message: ").encode('utf-8'))
-
-client.close()
-server.close()
+if __name__ == "__main__":
+    start_server()
